a7d2dcc84eba1c9e5692d305ee5b1c40cb2dc6ef
Maecenas Li <lx70716@gmail.com>
2018-12-07 13:05:47 +0800

    :sparkles: Introduce the process.env auto-loader

---
+++ config/default.js
@@ -0,0 +1,14 @@
+"use strict";
+
+module.exports = {
+  env: {
+    PORT: 3000
+  },
+  database: {
+    HOST: "mysql",
+    PORT: "3306",
+    USER: "example",
+    PASSWORD: "example",
+    DATABASE: "example"
+  }
+};
